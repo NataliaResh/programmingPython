962531fe562d14c508d37804cefd7cd3632227b9
Natalia Reshetnikova <nataliareshetnikova943@gmail.com>
2023-10-06 19:34:46 +0700

    Correct mintask04

--- mintask04.py
+++ mintask04.py
@@ -1,16 +1,14 @@
 d = {"a": 3, (1, 2): "a", (1, 3): "a", (1, 4): "a", "c": 1}
-repetitive_keys = {}
 unique_keys = {}
 reversed_dict = {}
 for key, value in d.items():
     if value in unique_keys:
-        repetitive_keys[value] = unique_keys[value], key
+        reversed_dict[value] = unique_keys[value], key
         unique_keys.pop(value)
-    elif value in repetitive_keys:
-        repetitive_keys[value] += key,
+    elif value in reversed_dict:
+        reversed_dict[value] += key,
     else:
         unique_keys[value] = key
-reversed_dict = repetitive_keys
 for key, value in unique_keys.items():
-    repetitive_keys[key] = value
+    reversed_dict[key] = value
 print(reversed_dict)
