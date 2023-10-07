185223945ba2fe8227fd1cd94a8799cdcf9c330e
Natalia Reshetnikova <nataliareshetnikova943@gmail.com>
2023-10-07 09:08:52 +0700

    Correcting mintask04 number 2

--- mintask04.py
+++ mintask04.py
@@ -1,14 +1,14 @@
 d = {"a": 3, (1, 2): "a", (1, 3): "a", (1, 4): "a", "c": 1}
-unique_keys = {}
+unique_keys = set()
 reversed_dict = {}
 for key, value in d.items():
     if value in unique_keys:
-        reversed_dict[value] = unique_keys[value], key
-        unique_keys.pop(value)
+        reversed_dict[value] = (reversed_dict[value], key)
+        unique_keys.remove(value)
     elif value in reversed_dict:
         reversed_dict[value] += key,
     else:
-        unique_keys[value] = key
-for key, value in unique_keys.items():
-    reversed_dict[key] = value
+        reversed_dict[value] = key
+        unique_keys.add(value)
+
 print(reversed_dict)
